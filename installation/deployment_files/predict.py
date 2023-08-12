64ee3527e4f2d879801e79cb89a70bc8717e07aa
Alexander Tretyak <alexander.mejinski@gmail.com>
2023-08-12 21:36:50 +0300

    refactor: linting; feature: added stack description

--- installation/deployment_files/predict.py
+++ installation/deployment_files/predict.py
@@ -4,25 +4,28 @@ import mlflow
 from flask import Flask, request, jsonify
 
 
-model_name = "sk-rfc-model"
+MODEL_NAME = "sk-rfc-model"
 
 mlflow.set_tracking_uri(os.getenv("MLFLOW_SITE_URL"))
-model = mlflow.pyfunc.load_model(model_uri=F"models:/{model_name}/latest")
+model = mlflow.pyfunc.load_model(model_uri=F"models:/{MODEL_NAME}/latest")
 
 app = Flask("red-wine-quality-prediction")
 
 
 def predict(features):
+    """Calls model prediction based on the json features."""
     return int(model.predict(features)[0])
 
 
 @app.route("/predict", methods=["POST"])
 def predict_endpoint():
+    """Creates a flask endpoint for prediction.
+    Returns prediction as a json."""
     wine = request.get_json()
     prediction = predict(wine)
     result = {
         "quality": prediction,
-        "model": model_name
+        "model": MODEL_NAME
     }
     return jsonify(result)
 
